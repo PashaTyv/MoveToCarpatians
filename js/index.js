5c7c648b0268a3fa04604f2bd80b37019442f20e
Pavlo <p.tyvonchuk@gmail.com>
2021-12-09 14:52:27 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper(".mySwiper", {
+   pagination: {
+     el: ".swiper-pagination",
+   },
+ });
