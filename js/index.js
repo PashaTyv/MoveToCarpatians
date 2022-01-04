e4436b204b14f809e82e5bdcbc001e8db38fa886
Pavlo <p.tyvonchuk@gmail.com>
2022-01-04 21:12:21 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,63 @@
-import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 
-const swiper = new Swiper(".mySwiper", {
-   pagination: {
-     el: ".swiper-pagination",
-   },
- });
+var swiper = new Swiper(".swiper-offers", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 70,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
+
+
+function changeSlide(index) {
+  const img = document.querySelectorAll('.img-swiper');
+  const text = document.querySelectorAll('.service-text');
+
+
+
+  for (let i = 0; i < img.length; i++) {
+    if (img[i].classList.contains('active') && text[i].classList.contains('active')) {
+      img[i].classList.remove('active');
+      text[i].classList.remove('active')
+    }
+
+  }
+
+  img[index].classList.add('active');
+  text[index].classList.add('active');
+}
+
+
+(function () {
+   const burger = document.querySelector('.header-burger');
+   const headerSideBar = document.querySelector('.header-sidebar');
+   burger.addEventListener('click', () => {
+   headerSideBar.classList.toggle('active');
+   burger.classList.toggle('close');
+   });
+}());
+
